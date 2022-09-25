24457215748dc109ac7148b735d0fcc35e199253
Miguelmorales13 <cacahuatisimo13@gmail.com>
2022-09-24 20:32:31 -0500

    add schema

--- src/databases/providers/sequelize-mysql.ts
+++ src/databases/providers/sequelize-mysql.ts
@@ -8,31 +8,24 @@ import {ImagesProduct} from "../../api/images-products/entities/images-product.e
 import {ClientsAddress} from "../../api/clients-addresses/entities/clients-address.entity";
 
 export const sequelizeMysqlProvider = [
-    // SequelizeModule.forRootAsync({
-    //     useFactory: () => ({
-    //         dialect: GetEnv("SEQUELIZE_DIALECT"),
-    //         host: GetEnv("SEQUELIZE_HOST"),
-    //         port: GetEnv("SEQUELIZE_PORT"),
-    //         username: GetEnv("SEQUELIZE_USERNAME"),
-    //         password: GetEnv("SEQUELIZE_PASSWORD"),
-    //         database: GetEnv("SEQUELIZE_DATABASE"),
-    //         schema: GetEnv("SEQUELIZE_DATABASE")
-    //     })
-    // }),
     {
         provide: "SEQUELIZE",
         useFactory: async () => {
             const sequelize = new Sequelize({
+
                 dialect: GetEnv("SEQUELIZE_DIALECT"),
                 host: GetEnv("SEQUELIZE_HOST"),
                 port: GetEnv("SEQUELIZE_PORT"),
                 username: GetEnv("SEQUELIZE_USERNAME"),
                 password: GetEnv("SEQUELIZE_PASSWORD"),
-                database: GetEnv("SEQUELIZE_DATABASE")
+                define: {
+                    schema: "e_commerce"
+                },
+                database: GetEnv("SEQUELIZE_DATABASE"),
             });
             sequelize.addModels([User, Client, CategoriesProduct, Product, ImagesProduct, ClientsAddress]);
 
-            await sequelize.sync({logging: true, alter: true, schema: GetEnv("SEQUELIZE_SCHEMA")});
+            await sequelize.sync({logging: true, alter: true});
             return sequelize;
         }
 
